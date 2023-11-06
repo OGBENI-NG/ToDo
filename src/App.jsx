d96ebc30e9dcba494e7687b6f6472f32a9e11273
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-11-06 14:53:55 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,12 @@ import { v4 as uuidv4 } from "uuid";
 import { FiCircle, FiCheckCircle } from "react-icons/fi";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 
-const TodoHeader = ({ formattedDate, inputValue, handleChange, addToDo }) => {
-  return (
-    <section>
-      <div className=" flex items-baseline justify-between pb-4">
-        <h1 className="capitalize text-5xl text-green-600 font-headerFont">
-          todo
-        </h1>
-        <h2 className="text-xl text-gray-500 font-bold">{formattedDate}</h2>
-      </div>
 
-      <section className="flex items-center gap-2 p-0 w-full">
-        <input
-          type="text"
-          name="text"
-          value={inputValue}
-          onChange={handleChange}
-          className="border-2 focus:border-none outline-3 focus:ring-green-200 focus:ring focus:outline-green-300 border-gray-300 w-full h-14 
-            rounded-xl p-2 px-3 text-2xl"
-        />
-        <button
-          onClick={addToDo}
-          className="uppercase h-14 px-4 text-xl rounded-xl font-bold text-white font-mainFont bg-green-600"
-        >
-          add
-        </button>
-      </section>
-    </section>
-  );
-};
 const TodoItem = ({ todo, onToggle, onDelete, deleteId}) => (
   <div
     onClick={() => onToggle(todo.id)}
     className={`${deleteId && "animate-myAnim"} flex items-start mb-5 bg-green-50 p-4 rounded-xl gap-3 ${
-      todo.isChecked && "bg-gray-300"
+      todo.isChecked && "bg-gray-300 "
     }`}
   >
     <div className="mt-1">
@@ -50,7 +22,7 @@ const TodoItem = ({ todo, onToggle, onDelete, deleteId}) => (
     </div>
     <p
       className={`text-green-700 text-2xl ${
-        todo.isChecked && "text-gray-600 line-through"
+        todo.isChecked && "line-through text-gray-600"
       }`}
     >
       {todo.text}
@@ -125,12 +97,31 @@ const App = () => {
 
   return (
     <main className="p-5 pt-8 h-screen w-full">
-      <TodoHeader
-        formattedDate={formattedDate}
-        handleChange={handleChange}
-        inputValue={inputValue}
-        addToDo={addToDo}
-      />
+       <section>
+      <div className="flex items-baseline justify-between pb-8 ">
+        <h1 className="capitalize text-5xl text-green-600 font-headerFont">
+          todo
+        </h1>
+        <h2 className="text-xl text-gray-500 font-semibold">{formattedDate}</h2>
+      </div>
+
+      <section className="flex items-center gap-2 p-0 w-full">
+        <input
+          type="text"
+          name="text"
+          value={inputValue}
+          onChange={handleChange}
+          className="border-2 focus:border-none outline-3 focus:ring-green-200 focus:ring focus:outline-green-300 border-gray-300 w-full h-14 
+            rounded-xl p-2 px-3 text-2xl"
+        />
+        <button
+          onClick={addToDo}
+          className="uppercase h-14 px-4 text-xl rounded-xl font-bold text-white font-mainFont bg-green-600"
+        >
+          add
+        </button>
+      </section>
+    </section>
       <div className="mt-5">{todoWrapper()}</div>
     </main>
   );
